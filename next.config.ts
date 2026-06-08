42606deed13f431518d9dce7b47d2789cfdabbab
Ahmad Feroz <ahmadferoz@example.com>
2026-06-08 17:03:10 +0800

    fix: redirect vercel subdomain to custom domain to prevent firebase auth errors

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,21 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'ppikkmk-portal.vercel.app',
+          },
+        ],
+        destination: 'https://ppikkmk.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
